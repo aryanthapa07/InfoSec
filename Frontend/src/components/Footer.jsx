9bb96a2b585e3294b0feb742399d899fd14b3e2b
aryanthapa07 <aryan.thapa@ganitinc.com>
2025-06-17 10:45:50 +0530

    footer

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
-export default function Footer() {
-  return (
-    <footer className="w-full bg-slate-50 text-center text-gray-500 py-4 text-sm mt-8 border-t border-gray-200">
-      Made with <span className="text-red-500 font-bold">AI</span> by Aryan Thapa (Product Team)
-    </footer>
-  );
-} 
+// export default function Footer() {
+//   return (
+//     // <footer className="w-full bg-slate-50 text-center text-gray-500 py-4 text-sm mt-8 border-t border-gray-200">
+//     //   Made with <span className="text-red-500 font-bold">AI</span> by Aryan Thapa (Product Team)
+//     // </footer>
+//   );
+// } 
